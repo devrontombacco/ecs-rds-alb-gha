e5a0498a74bcb72c9091c96b5cad3a0aed2375f2
Devron Tombacco <tombacco.devron@gmail.com>
2026-03-30 12:15:18 +0100

    Add all subnets

--- network.tf
+++ network.tf
@@ -26,3 +26,45 @@ resource "aws_subnet" "public2" {
     Name = "${var.vpc_name}-public2"
   }
 }
+
+resource "aws_subnet" "private1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.3.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = var.primary_az
+
+  tags = {
+    Name = "${var.vpc_name}-private1"
+  }
+}
+
+resource "aws_subnet" "private2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.4.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = var.secondary_az
+  tags = {
+    Name = "${var.vpc_name}-private2"
+  }
+}
+
+resource "aws_subnet" "private3" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.5.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = var.primary_az
+
+  tags = {
+    Name = "${var.vpc_name}-private3"
+  }
+}
+
+resource "aws_subnet" "private4" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.6.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = var.secondary_az
+  tags = {
+    Name = "${var.vpc_name}-private4"
+  }
+}
