281fe3ee713d0f9be81e8aefa6657002da2ad23d
Devron Tombacco <tombacco.devron@gmail.com>
2026-03-30 14:21:23 +0100

    Feat: add route tables

--- network.tf
+++ network.tf
@@ -77,3 +77,58 @@ resource "aws_subnet" "private4" {
     Name = "${var.vpc_name}-private4"
   }
 }
+
+
+resource "aws_route_table" "rt-public" {
+  vpc_id = aws_vpc.main-vpc.id
+
+  route {
+    cidr_block = "10.0.1.0/24"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  route {
+    cidr_block = "10.0.2.0/24"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.vpc_name}-rt-public"
+  }
+}
+
+resource "aws_route_table" "rt-private-primaryaz" {
+  vpc_id = aws_vpc.main-vpc.id
+
+  route {
+    cidr_block = "10.0.3.0/24"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  route {
+    cidr_block = "10.0.5.0/24"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.vpc_name}-rt-private-primaryaz"
+  }
+}
+
+resource "aws_route_table" "rt-private-secondaryaz" {
+  vpc_id = aws_vpc.main-vpc.id
+
+  route {
+    cidr_block = "10.0.4.0/24"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  route {
+    cidr_block = "10.0.6.0/24"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.vpc_name}-rt-private-secondaryaz"
+  }
+}
