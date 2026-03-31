66e6ff8fd87e42bf4577130c90f28a168ba0c83f
Devron Tombacco <tombacco.devron@gmail.com>
2026-03-31 11:53:06 +0100

    Fix: add target group to alb file

--- alb.tf
+++ alb.tf
@@ -1,1 +1,11 @@
+# Target Group
 
+resource "aws_lb_target_group" "tg" {
+  name        = "${var.vpc_name}-tg"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.main-vpc.id
+  target_type = "ip"
+}
+
+# 
