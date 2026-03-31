ae0f8d396ae0df2c57ae39fc18d44fa2f2f52b0b
Devron Tombacco <tombacco.devron@gmail.com>
2026-03-31 15:27:25 +0100

    Feat: add listeners to alb file

--- alb.tf
+++ alb.tf
@@ -30,3 +30,33 @@ resource "aws_lb" "alb" {
     Name = "${var.vpc_name}-alb"
   }
 }
+
+# ALB Listener
+
+resource "aws_lb_listener" "alb-listener-http" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_lb_listener" "alb-listener-https" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = aws_acm_certificate.cert.arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
