3a37031e51b17ee36153c2fcf83c9d178a790883
Devron Tombacco <tombacco.devron@gmail.com>
2026-06-11 20:12:11 +0100

    Add ecs service to ecs.tf

--- ecs.tf
+++ ecs.tf
@@ -36,3 +36,29 @@ resource "aws_ecs_cluster" "cluster" {
     value = "enabled"
   }
 }
+
+
+resource "aws_ecs_service" "flask-App" {
+  name            = "${var.vpc_name}-flask-app-service"
+  cluster         = aws_ecs_cluster.cluster.id
+  task_definition = aws_ecs_task_definition.service.arn
+  desired_count   = 2
+  depends_on      = [aws_lb_listener.alb-listener-https]
+  launch_type     = "FARGATE"
+  network_configuration {
+    subnets = [
+      aws_subnet.private1.id,
+      aws_subnet.private2.id,
+      aws_subnet.private3.id,
+      aws_subnet.private4.id
+    ]
+    security_groups  = [aws_security_group.sg-ecs.id]
+    assign_public_ip = false
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.tg.arn
+    container_name   = "flask-app"
+    container_port   = 5000
+  }
+}
