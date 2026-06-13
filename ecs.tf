93e4c280b368e8e7faa95ba179c2cf1ece1c48cf
Devron Tombacco <tombacco.devron@gmail.com>
2026-06-13 10:30:29 +0100

    remove redundant cpu and memory values from container level in task definition

--- ecs.tf
+++ ecs.tf
@@ -15,8 +15,6 @@ resource "aws_ecs_task_definition" "service" {
       name      = "flask-app"
       image     = "${data.aws_ecr_repository.flask_app.repository_url}:latest"
       essential = true
-      cpu       = 256
-      memory    = 512
       portMappings = [
         {
           containerPort = 5000
