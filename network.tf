318d0f5658eb9419c9adabb274f43b5634e5e86c
Devron Tombacco <tombacco.devron@gmail.com>
2026-07-11 15:47:21 +0200

    Fix: corrected gateway_id attribute error for public rt

--- network.tf
+++ network.tf
@@ -88,8 +88,8 @@ resource "aws_route_table" "rt-public" {
   vpc_id = aws_vpc.main-vpc.id
 
   route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_internet_gateway.igw.id
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
   }
 
   tags = {
