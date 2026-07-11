25705d51110559d60d6e5a2c4920e5e138c7d668
Devron Tombacco <tombacco.devron@gmail.com>
2026-07-11 15:38:50 +0200

    fix: fix routing error in rt-private-primaryaz

--- network.tf
+++ network.tf
@@ -103,13 +103,8 @@ resource "aws_route_table" "rt-private-primaryaz" {
   vpc_id = aws_vpc.main-vpc.id
 
   route {
-    cidr_block = "10.0.3.0/24"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-
-  route {
-    cidr_block = "10.0.5.0/24"
-    gateway_id = aws_internet_gateway.igw.id
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat1.id
   }
 
   tags = {
