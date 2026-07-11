c9e25080a85adb953d5cad1d74a2ccb0894d4751
Devron Tombacco <tombacco.devron@gmail.com>
2026-07-11 15:37:08 +0200

    fix: fix routing error in rt-public

--- network.tf
+++ network.tf
@@ -88,12 +88,7 @@ resource "aws_route_table" "rt-public" {
   vpc_id = aws_vpc.main-vpc.id
 
   route {
-    cidr_block = "10.0.1.0/24"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-
-  route {
-    cidr_block = "10.0.2.0/24"
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
 
