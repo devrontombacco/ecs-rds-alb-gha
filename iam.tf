f0750303468a3340339980f7410cb544907e722b
Devron Tombacco <tombacco.devron@gmail.com>
2026-08-07 14:59:01 +0200

    Feat: add deploy and deploy policy code to iam.tf

--- iam.tf
+++ iam.tf
@@ -1,6 +1,6 @@
-# -------------------------------------------------------
+# ------------------------------------------------------------------
 # ECS Task Execution Role (pull from ECR +  write to CloudWatch)
-# -------------------------------------------------------
+# ------------------------------------------------------------------
 
 resource "aws_iam_role" "ecs_task_execution_role" {
   name = "${var.vpc_name}-ecs-execution-role"
@@ -25,7 +25,7 @@ resource "aws_iam_role_policy_attachment" "ecs_execution_policy" {
 }
 
 # -------------------------------------------------------
-# ECS Task Role (for Flask app at runtime) 
+# ECS Task Role 
 # -------------------------------------------------------
 
 resource "aws_iam_role" "ecs_task_role" {
@@ -40,6 +40,104 @@ resource "aws_iam_role" "ecs_task_role" {
           Service = "ecs-tasks.amazonaws.com"
         }
         Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# ------------------------------------------------------------------
+# GitHub OIDC Provider
+# ------------------------------------------------------------------
+
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+}
+
+# ------------------------------------------------------------------
+# GitHub Actions Deploy Role (OIDC)
+# ------------------------------------------------------------------
+
+resource "aws_iam_role" "github_actions_deploy" {
+  name = "${var.vpc_name}-github-actions-deploy-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:YOUR_GITHUB_USERNAME/YOUR_REPO_NAME:ref:refs/heads/main"
+          }
+        }
+      }
+    ]
+  })
+}
+
+# ------------------------------------------------------------------
+# Permissions for deploy role
+# ------------------------------------------------------------------
+
+resource "aws_iam_role_policy" "github_actions_deploy_policy" {
+  name = "${var.vpc_name}-github-actions-deploy-policy"
+  role = aws_iam_role.github_actions_deploy.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr:GetAuthorizationToken"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:PutImage",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload"
+        ]
+        Resource = aws_ecr_repository.flask_app.arn
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "ecs:DescribeTaskDefinition",
+          "ecs:RegisterTaskDefinition"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "ecs:UpdateService",
+          "ecs:DescribeServices"
+        ]
+        Resource = aws_ecs_service.flask-App.id
+      },
+      {
+        Effect = "Allow"
+        Action = "iam:PassRole"
+        Resource = [
+          aws_iam_role.ecs_task_execution_role.arn,
+          aws_iam_role.ecs_task_role.arn
+        ]
       }
     ]
   })
