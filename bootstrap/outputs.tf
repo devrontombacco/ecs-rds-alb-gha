9d03249a629da3b93f56e64584c5d882ac557321
Devron Tombacco <tombacco.devron@gmail.com>
2026-08-15 16:34:41 +0200

    Fix: update description of state_bucket_name

--- bootstrap/outputs.tf
+++ bootstrap/outputs.tf
@@ -1,4 +1,4 @@
 output "state_bucket_name" {
-  description = "Name of the S3 bucket created for remote state — use this in the main project's backend.tf"
+  description = "S3 bucket created for remote state"
   value       = aws_s3_bucket.tf_state.id
 }
