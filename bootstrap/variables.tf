0f77308cd682da2c58cab48af0b29dfbd5e43991
Devron Tombacco <tombacco.devron@gmail.com>
2026-08-15 16:40:21 +0200

    Fix: update variable descriptions in variables.tf

--- bootstrap/variables.tf
+++ bootstrap/variables.tf
@@ -1,11 +1,11 @@
 variable "aws_region" {
-  description = "AWS region to create the state bucket (and lock table) in"
+  description = "AWS region for state bucket"
   type        = string
   default     = "eu-west-1"
 }
 
 variable "state_bucket_name" {
-  description = "Globally-unique S3 bucket name for Project 9's remote Terraform state"
+  description = "S3 bucket name"
   type        = string
 }
 
