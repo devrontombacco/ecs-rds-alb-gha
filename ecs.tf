36cf836fee7fb24a6b13b5befe3ef81b2f424751
Devron Tombacco <tombacco.devron@gmail.com>
2026-08-25 15:27:54 +0200

    Fix: add circuit breaker to ecs.tf file

--- ecs.tf
+++ ecs.tf
@@ -65,4 +65,9 @@ resource "aws_ecs_service" "flask-App" {
     container_name   = "flask-app"
     container_port   = 5000
   }
+
+  deployment_circuit_breaker {
+    enable   = true
+    rollback = true
+  }
 }
